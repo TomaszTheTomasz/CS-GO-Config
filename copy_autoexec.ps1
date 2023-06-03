edb5fbd4e4d5b65de47b2e513d015a29698cf2f7
Tomasz Anacki <tomasz.anacki@gmail.com>
2023-06-03 21:20:32 +0200

    Tweaks and script to copy

--- copy_autoexec.ps1
+++ copy_autoexec.ps1
@@ -1,2 +1,2 @@
 $CsGoPath = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 730").InstallLocation
-Copy-Item "$PSScriptRoot\autoexec.cfg" "$CsGoPath\csgo\cfg\autoexec.cfg"
+Copy-Item "$PSScriptRoot\*.cfg" "$CsGoPath\csgo\cfg\"
